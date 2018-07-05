103ec066bdb53be63dcc55221b26c18d73a5e7f3
Zachary Fried <friedz@oregonstate.edu>
2018-07-05 16:14:49 -0700

    locating matrix

--- R/compareStructures.R
+++ R/compareStructures.R
@@ -61,7 +61,8 @@ compareStructures <- function(structures, method=NULL, hausdorff.method=NULL, ve
 		}
 		par(mar=mar.old)
 	}
-#	return(contours)
+	return(contours)
+	print(lvl.i)
 }	
 
 compareStructures.surface <- function (structures) {	
