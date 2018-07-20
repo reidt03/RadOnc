ad4fe369e07e7715fd2d93b4023df2b63b4a4827
Zachary Fried <friedz@oregonstate.edu>
2018-07-20 15:05:27 -0700

    .

--- src/emd-rubner.c
+++ src/emd-rubner.c
@@ -255,7 +255,7 @@ float emd_rubner(signature_t *Signature1, signature_t *Signature2,
   free_globals();
   
   /* RETURN THE NORMALIZED COST == EMD */
-  printf(static float)(_C);
+  printf(static float)("%f", _C);
   return (float)(totalCost / w);
 }
 
