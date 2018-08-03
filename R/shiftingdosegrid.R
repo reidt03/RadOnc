72a336c79d9b02cafea3ad31b48bc7c6ea0fc3fd
localfry <friedz@oregonstate.edu>
2018-08-03 15:16:38 -0700

    added test RTdata

--- R/shiftingdosegrid.R
+++ R/shiftingdosegrid.R
@@ -1,4 +1,4 @@
-manEMD <- function(structure1, structure2, doseGrid){
+manEMD <- function(structure1, structure2, doseGrid){ 
     structure1weight <- matrix(c(rep(1, times = length(structure1)/3)))
     structure2weight <- matrix(c(rep(1, times = length(structure2)/3)))
     where.on.structure1 <- attributes(emdw(structure1, structure1weight, structure2, structure2weight, flows = TRUE))$flows[[1]] + 1;
@@ -12,17 +12,29 @@ manEMD <- function(structure1, structure2, doseGrid){
       directionVector <- c(Bpoint[1]-Apoint[1], Bpoint[2]-Apoint[2], Bpoint[3]-Apoint[3]) / distanceToTraverse #unit length vector pointing in direction of line
       fracVector <- directionVector * 0.1 #step length walking in line direction
       steps <- floor(distanceToTraverse /(fracVector[1]^2 + fracVector[2]^2 + fracVector[3]^2)^0.5) #how many steps (how many fracVectors fit inside of distanceToTraverse)
-      rise <- rep(NA, steps)
-    for (i in 1:steps) {
-        alpha <- Apoint + (i-0.5) * fracVector
-        beta <- Apoint + (i+0.5 )* fracVector
-        rise[i] <- abs(approx3D(doseGrid, x=(beta[1]), y=(beta[2]), z=(beta[3]), extrapolate = TRUE) - approx3D(doseGrid, x=(alpha[1]), y=(alpha[2]), z=(alpha[3]), extrapolate = TRUE))
+      if(distanceToTraverse !=  0){
+        rise <- rep(NA, steps)
+      }else{
+        iterations[i] <- 0
+        next
+      }
+    for (j in 1:steps) {
+        alpha <- Apoint + (j-0.5) * fracVector
+        beta <- Apoint + (j+0.5 )* fracVector
+        rise[j] <- abs(approx3D(doseGrid, x=(beta[1]), y=(beta[2]), z=(beta[3]), extrapolate = TRUE) - approx3D(doseGrid, x=(alpha[1]), y=(alpha[2]), z=(alpha[3]), extrapolate = TRUE))
     }
       iterations[i] <- sum(rise, na.rm=TRUE)
     }
     EMDres <- mean(iterations, na.rm = TRUE)
     return(EMDres)
 }
+
+manEMD(teeth1, teeth2, janedoe.RTdata$dose)
+manEMD(teeth1, teeth1, janedoe.RTdata$dose)
+manEMD(teeth1, teeth[[2]]$vertices, janedoe.RTdata$dose)
+manEMD(OG, OGToTheRight, RTdata$dose)
+manEMD(OG, OGToTheLeft, RTdata$dose)
+
 maketeeth1 <- function(x){
   teeth1 <- matrix(data = c(teeth[[1]]$vertices[,1], teeth[[1]]$vertices[,2] - 100, teeth[[1]]$vertices[,3]), 324, 3)
 }
@@ -32,9 +44,6 @@ maketeeth2 <- function(x){
 }
 # teeth1 <- matrix(data = c(teeth[[1]]$vertices[,1], teeth[[1]]$vertices[,2] - 100, teeth[[1]]$vertices[,3]), 324, 3)
 # teeth2 <- matrix(data = c(teeth[[2]]$vertices[,1], teeth[[2]]$vertices[,2] - 100, teeth[[2]]$vertices[,3]), 338, 3)
-# #manEMD(teeth1, teeth2)
-    
-
 
 
 
