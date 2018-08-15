1591c0a1d84d1987bbcf0fd550bc2f8b44550a0c
localfry <friedz@oregonstate.edu>
2018-08-15 11:25:46 -0700

    + EMD to cS

--- R/compareStructures.R
+++ R/compareStructures.R
@@ -23,7 +23,7 @@ compareStructures <- function(structures, method=NULL, hausdorff.method=NULL, ve
 		},
 		surface = contours <- compareStructures.surface(structures),
 		hausdorff = return(compareStructures.hausdorff(structures, method=hausdorff.method, verbose=verbose)),
-		EMD = {
+		EMD =  {
 		  contours <- compareStructures.axial(structures, pixels=pixels)
 		  N <- dim(contours)[2]-3
 		  results <- matrix(0, nrow=N, ncol=N, dimnames=list(names(structures), names(structures)))
@@ -33,7 +33,11 @@ compareStructures <- function(structures, method=NULL, hausdorff.method=NULL, ve
 		        results[i, j] <- 1#
 		        next
 		      }
-		      results[i, j] <- manEMD(structure1 = structures[i], structure2 = structures[j], doseGrid = dgrid)
+		      if(results[i,j]>0){
+		        next()
+		      }
+		      results[i, j] <- manEMD(structure1 = structures[[i]]$vertices, structure2 = structures[[j]]$vertices, doseGrid = dgrid) + 1
+		      results[j, i] <- results[i, j]
 		    }
 		  }
 		  return(results)
@@ -75,9 +79,8 @@ compareStructures <- function(structures, method=NULL, hausdorff.method=NULL, ve
 		}
 		par(mar=mar.old)
 	}
-	return(contours)
+	#return(contours)
 }	
-
 
 compareStructures.surface <- function (structures) {	
 	N <- length(structures)
@@ -209,46 +212,3 @@ pointInPoly2D <- function (points, poly) {
 		return(pip2d(poly, points) >= 0)
 	}
 }
-
-manEMD <- function(structure1, structure2, doseGrid){ 
-  if(is.null(doseGrid)){
-    print("You must input a dose grid")
-    stop()
-  }
-  structure1weight <- matrix(c(rep(1, times = length(structure1)/3)))
-  structure2weight <- matrix(c(rep(1, times = length(structure2)/3)))
-  where.on.structure1 <- attributes(emdw(structure1, structure1weight, structure2, structure2weight, flows = TRUE))$flows[[1]] + 1;
-  where.on.structure2 <- attributes(emdw(structure1, structure1weight, structure2, structure2weight, flows = TRUE))$flows[[2]] + 1;
-  iterations <- rep(NA, length(where.on.structure1))
-  for (i in 1:length(where.on.structure1)) {
-    Apoint <- structure1[where.on.structure1[i],]
-    Bpoint <- structure2[where.on.structure2[i],]
-    distanceToTraverse <- ((Bpoint[1]-Apoint[1])^2 + (Bpoint[2]-Apoint[2])^2 + (Bpoint[3] - Apoint[3])^2)^.5 #total distance between starting and ending points
-    distTrav <- c((Bpoint[1]-Apoint[1]), (Bpoint[2]-Apoint[2]), (Bpoint[3] - Apoint[3])) #FIX DIVIDE BY ZERO ERROR FOR STEPS
-    directionVector <- c(Bpoint[1]-Apoint[1], Bpoint[2]-Apoint[2], Bpoint[3]-Apoint[3]) / distanceToTraverse #unit length vector pointing in direction of line
-    fracVector <- directionVector * 0.1 #step length walking in line direction
-    steps <- floor(distanceToTraverse /(fracVector[1]^2 + fracVector[2]^2 + fracVector[3]^2)^0.5) #how many steps (how many fracVectors fit inside of distanceToTraverse)
-    if(distanceToTraverse !=  0){
-      rise <- rep(NA, steps)
-    }else{
-      iterations[i] <- 0
-      next
-    }
-    for (j in 1:steps) {
-      alpha <- Apoint + (j-0.5) * fracVector
-      beta <- Apoint + (j+0.5 )* fracVector
-      rise[j] <- abs(approx3D(doseGrid, x=(beta[1]), y=(beta[2]), z=(beta[3]), extrapolate = TRUE) - approx3D(doseGrid, x=(alpha[1]), y=(alpha[2]), z=(alpha[3]), extrapolate = TRUE))
-    }
-    iterations[i] <- sum(rise, na.rm=TRUE)
-  }
-  EMDres <- mean(iterations, na.rm = TRUE)
-  return(EMDres)
-}
-
-
-
-
-
-
-
-
